158d1c83a48aee0e9e58562a3785989382fbb6b8
kunal kathore <kunalkathore95@gmail.com>
2026-09-19 14:24:04 +0530

    modified:   Basic_project/action.js

--- Basic_project/action.js
+++ Basic_project/action.js
@@ -2,8 +2,9 @@ function switchOff() {
   document.getElementById("bulbImage").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
   document.getElementById("catImage").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
   document.getElementById("switchStatus").textContent = "Switched Off";
-  document.getElementById("onSwitch").style.backgroundColor = "#22c55e";
   document.getElementById("offSwitch").style.backgroundColor = "#cbd2d9";
+    document.getElementById("onSwitch").style.backgroundColor = "#22c55e";
+
 }
 
 function switchOn() {
@@ -13,3 +14,4 @@ function switchOn() {
   document.getElementById("offSwitch").style.backgroundColor = "#e12d39";
   document.getElementById("onSwitch").style.backgroundColor = "#cbd2d9";
 }
+
